eb702ffa04a72b261c522eedfa1fdfc93d91c54a
Nico Elbers <nico.b.elbers@gmail.com>
2025-12-25 00:21:36 +0100

    Build: add check step

--- build.zig
+++ build.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    // const optimize = b.standardOptimizeOption(.{});
 
     const mod = b.addModule("ser", .{
         .root_source_file = b.path("src/root.zig"),
@@ -9,5 +10,9 @@ pub fn build(b: *std.Build) void {
     });
 
     const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&(b.addRunArtifact(b.addTest(.{ .root_module = mod }))).step);
+    const mod_tests = b.addTest(.{ .root_module = mod });
+    test_step.dependOn(&(b.addRunArtifact(mod_tests)).step);
+
+    const check_step = b.step("check", "Check if everything compiles");
+    check_step.dependOn(&mod_tests.step);
 }
